5b99b0b2570bc6b3631d7b01b82424db1a3bc9a8
NicoADavite <nicolasalceodavite@gmail.com>
2022-03-23 18:44:08 -0300

    modificando usuario admin

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -40,7 +40,7 @@ function TopBar(){
 						{/*<!-- Nav Item - User Information -->*/}
 						<li className="nav-item dropdown no-arrow">
 							<a className="nav-link dropdown-toggle" href="/" id="userDropdown">
-								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Julito Marchelo de la EEENNNEEEBBEEAAAAA</span>
+								<span className="mr-2 d-none d-lg-inline text-gray-600 small">Usuario Administrador</span>
 								<img className="img-profile rounded-circle" src={admin} alt="Mickel Jordan - NBA" width="200"/>
 							</a>
 						</li>
